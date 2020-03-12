2c51774590390fb3a80b260df7f0f87188ee5156
Axel Mukwena <axel.muk@gmail.com>
2020-03-13 01:33:11 +0800

    Define settings intent, define setting activity

--- app/src/main/java/com/example/celebrityquiz/MainActivity.java
+++ app/src/main/java/com/example/celebrityquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.celebrityquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
@@ -21,17 +22,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
-        case R.id.play:
-            return(true);
-
-
-    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.play:
+                return(true);
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                this.startActivity(intent);
+                return(true);
+        }
         return(super.onOptionsItemSelected(item));
     }
 }
