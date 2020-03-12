59df0658cf37aefda7de327e19718c2a12c5b6ce
Axel Mukwena <axel.muk@gmail.com>
2020-03-12 20:46:03 +0800

    Toolbar UI icons

--- app/src/main/java/com/example/celebrityquiz/MainActivity.java
+++ app/src/main/java/com/example/celebrityquiz/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,4 +17,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
+        case R.id.play:
+            return(true);
+
+
+    }
+        return(super.onOptionsItemSelected(item));
+    }
 }
+
